ee2986d0d8cc1d9d8b732c513acd6a3e92f68e73
Yu Hao Huang <sam821203@yahoo.com.tw>
2022-01-04 09:54:15 +0800

    font awesome check

--- tpl/foot.php
+++ tpl/foot.php
@@ -1,19 +1,21 @@
     
+    <!-- JavaScript Bundle with Popper -->
 
     <!-- jQuery 3.6.0 -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script> 
 
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+    <!-- jQuery UI -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 
-    <!-- Masonry -->
-    <script src="/masonry-docs/masonry.pkgd.min.js"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script> -->
+
+    <!-- bootstrap 4.6.0 -->
+    <link rel="stylesheet" href="./js/bootstrap.bundle.min.js">
 
     <!-- Font awesome -->
-    <!-- 問題：為何不能只放 all.min.js? -->
-    <!-- <script src="js/awesome.all.min.js"></script> -->
-    <script src="https://kit.fontawesome.com/1392152695.js" crossorigin="anonymous"></script>
-
+    <script src="./js/fontawesome.all.min.js"></script>
+    
+    <!-- Movinon navbar -->
     <script src="./js/movinon-navbar.js"></script>
 
     
--- tpl/head.php
+++ tpl/head.php
@@ -6,17 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movin'on</title>
 
-    <!-- Bootstrap 4.6.0 -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    
     <!-- jQuery UI -->
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
 
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Bootstrap 5.1.3 -->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
+    
+    <!-- Bootstrap 4.6.0 -->
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
 
     <!-- Font awesome -->
-    <link rel="stylesheet" href="css/awesome.all.min.css">
+    <link rel="stylesheet" href="./css/fontawesome.all.min.css">
     
     <!-- Google font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
